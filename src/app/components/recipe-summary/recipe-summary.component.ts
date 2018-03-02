3811cb8614cd114afca26d391af8048320ad1ab4
YKSONG\USER <pkleon78@gmail.com>
2018-03-02 18:06:25 +0900

    Initial commit

---
+++ src/app/components/recipe-summary/recipe-summary.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input, EventEmitter, Output} from '@angular/core';
+import { Recipe } from '../../model/recipe';
+
+@Component({
+  selector: 'app-recipe-summary',
+  templateUrl: './recipe-summary.component.html',
+  styleUrls: ['./recipe-summary.component.css']
+})
+export class RecipeSummaryComponent  {
+
+  @Input()
+  recipe: Recipe;
+
+  @Output()
+  zoomIn: EventEmitter<Recipe> = new EventEmitter();
+
+  constructor() {
+  }
+
+  public zoomClicked() {
+    this.zoomIn.emit(this.recipe);
+  }
+
+}
